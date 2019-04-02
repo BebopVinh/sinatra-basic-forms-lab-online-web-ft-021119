2f63fa17999c3ca14ead009d0e1de6eb6cafe560
Vinh Do <vinhdo07@gmail.com>
2019-04-02 21:12:39 +0000

    Automatically backed up by Learn

--- models/puppy.rb
+++ models/puppy.rb
@@ -1,6 +1,7 @@
 class Puppy
-   attr_reader :breed 
+   attr_reader :breed
    attr_accessor :name, :months_old
+   
    def initialize(name: nil, breed: nil, months_old: nil)
       @name = name
       @breed = breed
